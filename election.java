56264a22c8d12f9c45721b2b37ae1f2fb8c7bea5
tgonzalezdiuk <tamara.gsx@gmail.com>
2024-03-05 14:58:46 -0500

    14:58 03/05/24

--- election.java
+++ election.java
@@ -4,20 +4,22 @@ Date: 03/04/24
 Program: Java
  */
 
- import jav.util.*;
+ import java.util.*;
  public class election 
 {
      public static void main(String[] args )
      {
-         Scanner.input = new Scanner(Sytem.im);
-         System.out.print("Type your name and press Enter: ");
+         Scanner input = new Scanner(System.in);
+         System.out.print("How many votes did Awbrey get? Press Enter: ");
+         String AwbreyVotes = input.nextLine();
+         System.out.println("Candidate\t Votes Percentage\nAwbrey\t " + AwbreyVotes);
          
-         String username = input.nextLine();
-         System.out.println("Hello " + username);
-         
-         System.out.print("Type your city and press Enter: ");
-         String cityName = input.nextLine();
-         System.out.println("You live in " + cityName);
-         
+         System.out.print("How many votes did Awbrey get? Press Enter: ");
+         String MartinezVotes = input.nextLine();
+         System.out.println("Martines\t " + MartinezVotes);
+
+         System.out.println("TOTAL VOTES");
+
          input.close();
+     }
  }
